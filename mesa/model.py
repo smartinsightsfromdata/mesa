422845438d24c8994f7a1b10ed8eb977885c2256
Corvince <vincent.hess@uni-graz.at>
2017-02-05 23:08:12 +0100

    Added add-agent and remove-agent functionality

--- mesa/model.py
+++ mesa/model.py
@@ -7,6 +7,7 @@ Core Objects: Model
 """
 import datetime as dt
 import random
+from mesa.agent import Agent
 
 
 class Model:
@@ -42,3 +43,30 @@ class Model:
     def step(self):
         """ A single step. Fill in here. """
         pass
+
+    def add_agent(self, agent, pos='random', schedule=True):
+        """ Add an agent to the model and possibly adds it to the grid and the scheduler. Possible Arguments:
+
+        agent: Agent to be added to the model
+        pos: The position of the model. Only used if the model has a grid
+        If "random" is passed, place the agent at random position. Explicitly pass "pos=None" if you want to add an agent without a position
+        schedule: True/False. If True add the agent to the models schedule
+        """
+        agent.model = self
+        if self.grid:
+            if pos == 'random':
+                x = random.randint(0, self.grid.width)
+                y = random.randint(0, self.grid.height)
+                pos = (x, y)
+            self.grid.place_agent(pos)
+        if schedule:
+            self.schedule.add(agent)
+
+    def remove_agent(self, agent):
+        """ Remove an agent from the model, the grid and its scheduler
+        """
+        agent.model = None
+        if agent in self.schedule.agents:
+            self.schedule.remove(agent)
+        if self.grid:
+            self.grid.remove_agent(agent)
